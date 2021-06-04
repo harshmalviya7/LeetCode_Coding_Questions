a442bf962a77b07b798b6b5be5b46d3625dc6a95
Harshmalviya7 <harsh.malviya.9869@gmail.com>
2021-06-04 17:16:47 +0530

    Leetcode 387

--- Firstuniquecharcterinstring.py
+++ Firstuniquecharcterinstring.py
@@ -2,18 +2,19 @@
 # https://leetcode.com/problems/first-unique-character-in-a-string/
 
 
-
 class Solution:
-    def firstUniqChar(self, s: str) -> int: d={}
+    def firstUniqChar(self, s: str) -> int:
+        d = {}
         for i in range(len(s)):
             if s[i] not in d:
-                d[s[i]]=i
+                d[s[i]] = i
             else:
-                d[s[i]]=-1
+                d[s[i]] = -1
+        min_no = 100000
         for i in d:
-            if d[i]!=-1:
-                return d[i]
-        return -1
+            if d[i] != -1 and d[i] < min_no:
+                min_no = d[i]
+        return -1 if min_no == 100000 else min_no
 
 
-
+ob=Solution()
