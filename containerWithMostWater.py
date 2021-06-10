d80f6244a200cbf8a551f5a48b83e8916a2ad589
Harshmalviya7 <harsh.malviya.9869@gmail.com>
2021-06-10 19:43:51 +0530

    Leetcode 11

--- containerWithMostWater.py
+++ containerWithMostWater.py
@@ -1,5 +1,21 @@
 # 11. Container With Most Water
 # https://leetcode.com/problems/container-with-most-water/
+# class Solution:
+#     def maxArea(self, height: List[int]) -> int:
+#
+#         a = 0
+#         i = 0
+#         j = len(height) - 1
+#         while (i < j):
+#
+#             b = min(height[i], height[j])
+#             a = max(a, b * (j - i))
+#             if height[i] < height[j]:
+#                 i += 1
+#             else:
+#                 j -= 1
+#
+#         return a
 class Solution:
     def maxArea(self, height: List[int]) -> int:
 
@@ -8,11 +24,11 @@ class Solution:
         j = len(height) - 1
         while (i < j):
 
-            b = min(height[i], height[j])
-            a = max(a, b * (j - i))
             if height[i] < height[j]:
+                b = (height[i]) * (j - i)
                 i += 1
             else:
+                b = (height[j]) * (j - i)
                 j -= 1
-
+            a = max(a, b)
         return a
