ae50e6b1bd6d26d0c69d138b5f62bcff36e4312a
Harshmalviya7 <harsh.malviya.9869@gmail.com>
2021-06-15 14:34:03 +0530

    leetcode 973

---
+++ kClosestPointsToOrigin.py
@@ -0,0 +1,14 @@
+# 973. K Closest Points to Origin
+# https://leetcode.com/problems/k-closest-points-to-origin/
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        ou t =0 e=[]
+        for j,i in enu merate(points):
+
+            a=(i[0]
+            ** 2+ i[ 1] **2)**0.5
+            e.appe nd([a,i])
+
+        return [x[1] for x in sorted(e)][:k]
+
+
