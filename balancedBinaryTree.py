0d593a2e1a0ba43ae26c699dd87e0950f718c663
Harshmalviya7 <harsh.malviya.9869@gmail.com>
2021-06-25 01:34:57 +0530

    leetcode 110

--- balancedBinaryTree.py
+++ balancedBinaryTree.py
@@ -12,18 +12,23 @@ class Solution:
         if root is None:
             return True
 
-
         def height(root):
             if root is None:
-                return 0 l=height(root. left)
-            r=height(root . right)
-            if l==-1 or r==- 1:
+                return 0
+            l = height(root.left)
+            r = height(root.right)
+            if l == -1 or r == -1:
                 return -1
-            if abs(r-l)>1:
+            if abs(r - l) > 1:
                 return -1
-            return 1+max(l,r) if height(root)!=-1:
-            retu rn True
+            return 1 + max(l, r)
+
+        if height(root) != -1:
+            return True
         else:
             return False
+
+
+
 
 
