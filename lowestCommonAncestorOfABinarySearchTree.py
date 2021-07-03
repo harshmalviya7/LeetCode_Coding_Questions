b500eed1944c3bc71baf6f00859870241b8da350
Harshmalviya7 <harsh.malviya.9869@gmail.com>
2021-07-03 12:48:31 +0530

    leetcode 235

--- lowestCommonAncestorOfABinarySearchTree.py
+++ lowestCommonAncestorOfABinarySearchTree.py
@@ -11,8 +11,8 @@
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
         if p.val<root.val and q.val<root.val:
-            return lowestCommonAncestor(root.left,p,q)
+            return self.lowestCommonAncestor(root.left,p,q)
         elif p.val>root.val and q.val>root.val:
-            return lowestCommonAncestor(root.right,p,q)
+            return self.lowestCommonAncestor(root.right,p,q)
         else:
             return root
