732d02e49d64ab5c1e316458bfb2e3a2180c2ddb
Harshmalviya7 <harsh.malviya.9869@gmail.com>
2021-07-07 12:54:58 +0530

    leetcode 459

---
+++ repeatedSubStringPattern.py
@@ -0,0 +1,15 @@
+# 459. Repeated Substring Pattern
+# https://leetcode.com/problems/repeated-substring-pattern/
+
+class Solution:
+    def repeatedSubstringPattern(self, s: str) -> bool:
+
+        res = ""
+        n = len(s)
+        for i in range(n // 2):
+            res += s[i]
+            if n % len(res) == 0:
+                if res * (n // len(res)) == s:
+                    return True
+        return False
+
