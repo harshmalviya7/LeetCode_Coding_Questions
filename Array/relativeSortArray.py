4923149862d22889376c7d6d549a23e5a7375004
Harshmalviya7 <harsh.malviya.9869@gmail.com>
2021-08-15 12:27:08 +0530

    leetcode 1122

---
+++ Array/relativeSortArray.py
@@ -0,0 +1,17 @@
+# https://leetcode.com/problems/relative-sort-array/
+# 1122. Relative Sort Array
+from collections import Counter
+class Solution:
+    def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
+
+        d1 = Counter(arr1)
+
+        l = []
+        for i in (arr2):
+            l += [i] * d1[i]
+            del d1[i]
+
+        for i in sorted(d1):
+            l += [i] * d1[i]
+
+        return l
