79e75481ca285b85ee7900e8f99147f22539856c
harshmalviya7 <52200329+harshmalviya7@users.noreply.github.com>
2021-09-17 17:55:55 +0530

    leetcode 1903

---
+++ String/largestOddNumberInString.py
@@ -0,0 +1,26 @@
+# 1903. Largest Odd Number in String
+# https://leetcode.com/problems/largest-odd-number-in-string/
+'''
+Example 1:
+
+Input: num = "52"
+Output: "5"
+Explanation: The only non-empty substrings are "5", "2", and "52". "5" is the only odd number.
+Example 2:
+
+Input: num = "4206"
+Output: ""
+Explanation: There are no odd numbers in "4206".
+Example 3:
+
+Input: num = "35427"
+Output: "35427"
+Explanation: "35427" is already an odd number.
+'''
+class Solution:
+    def largestOddNumber(self, num: str) -> str:
+
+        for i in range(len(num) - 1, -1, -1):
+            if int(num[i]) % 2 != 0:
+                return num[:i + 1]
+        return ""
